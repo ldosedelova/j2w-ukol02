ed7ef292cad890e92c35658dfab4c7697abba510
Lenka Dosedělová <dosedelova.l@seznam.cz>
2023-09-30 19:57:16 +0200

    With list of texts from readAllLines

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -3,30 +3,38 @@ package cz.czechitas.java2webapps.ukol2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
     private final Random random = new Random();
 
     @GetMapping("/")
-    public ModelAndView dynamicPage(){
+    public ModelAndView dynamicPage() throws IOException {
         int nahodneCisloObrazek = random.nextInt(9);
         int nahodneCisloText = random.nextInt(8);
+        List<String> seznamTextu = readAllLines("citaty.txt");
         ModelAndView result = new ModelAndView("index");
         result.addObject("obrazek",String.format("images/Foto_%d.jpg",nahodneCisloObrazek));
         result.addObject("text", seznamTextu.get(nahodneCisloText));
         return result;
     }
 
-    List<String> seznamTextu=List.of(
-            "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
-            "A user interface is like a joke. If you have to explain it, it's not that good.",
-            "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
-            "I have a joke on programming but it only works on my computer.",
-            "99 little bugs in the code, 99 bugs in the code. Take one down, patch it around. 127 little bugs in the code…",
-            "When I wrote this code, only God & I understood what it did. Now… Only God knows.",
-            "Programmer (noun.): A machine that turns coffee into code.",
-            "Real programmers count from 0.");
+    private static List<String> readAllLines(String resource)throws IOException {
+        ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+        try(InputStream inputStream=classLoader.getResourceAsStream(resource);
+            BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+            return reader
+                    .lines()
+                    .collect(Collectors.toList());
+        }
+    }
 }
